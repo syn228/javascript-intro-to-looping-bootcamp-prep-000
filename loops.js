e970a895a0f0c2e3806f4b5dcecbbfcbcbccce49
Aaron Na <syn228@nyu.edu>
2018-04-09 18:21:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,5 @@
 function forLoop(array){
+<<<<<<< HEAD
   for (var i = 0; i < 25; i++){
     if (i === 1){
       array.push(`I am ${i} strange loop.`)
@@ -11,10 +12,22 @@ function whileLoop(n){
     let countdown = n
     while (countdown > 0){
       console.log(--countdown)
+=======
+  for (let i = 0; i < 25; i++){
+    console.log(`I am ${i} strange loop${i <= 1 ? '' : 's'}.`)
+  }
+  return i
+}
+function whileLoop(n){
+  let countdown = n
+  while (countdown > 0){
+    console.log(--countdown)
+>>>>>>> 11463beebe689ce19ad1809cb4c0e84b0d9e2ccb
   }
   return 'done'
 }
 function doWhileLoop(array){
+<<<<<<< HEAD
   function maybeTrue() {
   return Math.random() >= 0.5
 }
@@ -22,4 +35,7 @@ function doWhileLoop(array){
     console.log(--array.length)
   } while (array.length > 0 && maybeTrue());
   return array
+=======
+  
+>>>>>>> 11463beebe689ce19ad1809cb4c0e84b0d9e2ccb
 }
